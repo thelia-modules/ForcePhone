c0d58d30a93d1d7e2bbc52bfc4ea9dd5e100ab27
Nicolas Barbey <223106@supinfo.com>
2021-09-14 16:47:18 +0200

    Add command to update all phone numbers

---
+++ Command/UpdatePhoneNumberCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace ForcePhone\Command;
+
+
+
+use libphonenumber\PhoneNumberFormat;
+use libphonenumber\PhoneNumberUtil;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Thelia\Command\ContainerAwareCommand;
+use Thelia\Model\Base\AddressQuery;
+
+class UpdatePhoneNumberCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName("module:ForcePhone:update")
+            ->setDescription("Update phone number of all addresses");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $addresses = AddressQuery::create()->find();
+
+        foreach ($addresses as $address){
+            if ($phone = $address->getPhone()){
+                $address
+                    ->setPhone($this->updatePhone($phone, $address->getCountry()->getIsoalpha2()))
+                    ->save();
+            }
+            if ($cellphone = $address->getCellphone()){
+                $address
+                    ->setCellphone($this->updatePhone($cellphone, $address->getCountry()->getIsoalpha2()))
+                    ->save();
+            }
+        }
+
+        $output->writeln('Success');
+
+        return 0;
+    }
+
+    protected function updatePhone($phoneNumber, $isoalpha2)
+    {
+        $phoneUtil = PhoneNumberUtil::getInstance();
+
+        try {
+            $phoneNumberProto = $phoneUtil->parse($phoneNumber, $isoalpha2);
+        }catch (\Exception $e){
+            return null;
+        }
+
+        if ($phoneUtil->isValidNumber($phoneNumberProto)) {
+            return $phoneUtil->format($phoneNumberProto, PhoneNumberFormat::INTERNATIONAL);
+        }
+        return $phoneNumber;
+    }
+}
