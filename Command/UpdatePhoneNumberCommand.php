8064e2892c45cf5b145e8b99e8fda361025a8a0d
Nicolas Barbey <223106@supinfo.com>
2021-09-15 17:18:06 +0200

    Add warning to command

--- Command/UpdatePhoneNumberCommand.php
+++ Command/UpdatePhoneNumberCommand.php
@@ -10,7 +10,9 @@ use libphonenumber\PhoneNumberUtil;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Thelia\Command\ContainerAwareCommand;
+use Thelia\Log\Tlog;
 use Thelia\Model\Base\AddressQuery;
+use Thelia\Model\Customer;
 
 class UpdatePhoneNumberCommand extends ContainerAwareCommand
 {
@@ -27,27 +29,35 @@ class UpdatePhoneNumberCommand extends ContainerAwareCommand
 
         foreach ($addresses as $address){
             if ($phone = $address->getPhone()){
-                $address
-                    ->setPhone($this->updatePhone($phone, $address->getCountry()->getIsoalpha2()))
-                    ->save();
+                $phone = $this->updatePhone($phone, $address->getCountry()->getIsoalpha2(), $address->getCustomer(), $output);
+                if ($phone !== null){
+                    $address
+                        ->setPhone($phone)
+                        ->save();
+                }
             }
             if ($cellphone = $address->getCellphone()){
-                $address
-                    ->setCellphone($this->updatePhone($cellphone, $address->getCountry()->getIsoalpha2()))
-                    ->save();
+                $cellphone = $this->updatePhone($cellphone, $address->getCountry()->getIsoalpha2(), $address->getCustomer(), $output);
+                if ($cellphone !== null) {
+                    $address
+                        ->setCellphone($cellphone)
+                        ->save();
+                }
             }
         }
 
         $output->writeln('Success');
     }
 
-    protected function updatePhone($phoneNumber, $isoalpha2)
+    protected function updatePhone($phoneNumber, $isoalpha2, Customer $customer, OutputInterface $output)
     {
         $phoneUtil = PhoneNumberUtil::getInstance();
 
         try {
             $phoneNumberProto = $phoneUtil->parse($phoneNumber, $isoalpha2);
         }catch (\Exception $e){
+            Tlog::getInstance()->error('Phone number '.$phoneNumber.' for customer '.$customer->getRef().' is invalid');
+            $output->writeln('Error : Phone number '.$phoneNumber.' for customer '.$customer->getRef().' is invalid');
             return null;
         }
 
