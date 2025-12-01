35a0cd61e63600c84f160d96b238a8d57ab46993
Thomas Da Silva <tdasilva@openstudio.fr>
2025-12-01 10:00:32 +0100

    Maj Twig

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -3,12 +3,15 @@
 return array(
     'At least one phone number' => 'Au moins un des deux',
     'Cellphone' => 'Téléphone mobile',
+    'Check phone numbers format' => 'Vérifier le format de la saisie du téléphone',
     'ForcePhone configuration' => 'Configuration de Force Phone',
     'Home phone number' => 'Numéro de téléphone fixe',
+    'Invalid cellphone number.' => 'Numéro de téléphone mobile invalide.',
+    'Invalid phone number.' => 'Numéro de téléphone invalide.',
     'Mobile phone number' => 'Numéro de téléphone mobile',
+    'No cellphone number found' => 'Pas de numéro de téléphone mobile trouvé.',
+    'No phone number found' => 'Pas de numéro de téléphone trouvé.',
     'Phone' => 'Téléphone fixe',
-    'Please enter a home or mobile phone number' => 'Merci d\'indiquer un numéro de téléphone fixe ou mobile',
-    'Please enter at least one phone number.' => 'Merci d\'indiquer au moins un numéro de téléphone',
-    'Check phone numbers format' => 'Vérifier le format de la saisie du téléphone',
     'Please enter a valid phone number' => 'Merci d\'indiquer un numéro de téléphone valide',
+    'Please enter at least one phone number.' => 'Merci d\'indiquer au moins un numéro de téléphone',
 );
