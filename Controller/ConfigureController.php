4a1bdb2d2a7a854dba74d6ec65b29f25f761165c
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-30 11:44:55 +0200

    fix: replace deprecated Request::get() with explicit bag access

--- Controller/ConfigureController.php
+++ Controller/ConfigureController.php
@@ -57,7 +57,8 @@ class ConfigureController extends BaseAdminController
             );
 
             // Redirect to the success URL,
-            if ($requestStack->getCurrentRequest()?->get('save_mode') === 'stay') {
+            $request = $requestStack->getCurrentRequest();
+            if ($request?->attributes->get('save_mode', $request->query->get('save_mode', $request->request->get('save_mode'))) === 'stay') {
                 // If we have to stay on the same page, redisplay the configuration page/
                 $url = '/admin/module/ForcePhone';
             } else {
